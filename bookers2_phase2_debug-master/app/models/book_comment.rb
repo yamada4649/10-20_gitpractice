231eb87b0a5b7ad4b339619976216825b797951d
EC2 Default User <ec2-user@ip-172-31-52-82.ec2.internal>
2021-10-20 11:16:36 +0000

    Revert "mainブランチでpushしています。"
    This reverts commit e606303124667ce4210f17120153ffb387f3dca2.

--- bookers2_phase2_debug-master/app/models/book_comment.rb
+++
@@ -1,5 +0,0 @@
-class BookComment < ApplicationRecord
-  belongs_to :book
-  belongs_to :user
-  validates :body, presence: true
-end
--- bookers2_phase2_debug-master/app/models/favorite.rb
+++
@@ -1,4 +0,0 @@
-class Favorite < ApplicationRecord
-  belongs_to :book
-  belongs_to :user
-end
